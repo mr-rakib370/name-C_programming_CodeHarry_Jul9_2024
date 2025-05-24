e3cb82db4bd461bbd310f00bfed0f23e4e4b2f0f
Mr. Rakib <mrrakib0143@gmail.com>
2025-05-24 22:22:18 +0600

    Create C_programming_variables.c

---
+++ C_programming_variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main(){
+        int a; // variable declaration
+        a = 1; //variable initialization
+        printf("%d",a);
+                
+        
+        return 0;
+
+    }
